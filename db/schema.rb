af85f56318705f783066ccbade06e3c8d61b17cf
naoto <naotwo28@gmail.com>
2024-01-22 22:09:23 +0900

    Add: postモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_19_050416) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_21_185450) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "song_title", null: false
+    t.text "lyricks", null: false
+    t.string "artist", null: false
+    t.text "coment"
+    t.string "link_to_music"
+    t.integer "design", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +39,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_19_050416) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
