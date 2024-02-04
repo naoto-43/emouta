747357b3b8eaca9db7bcf841f01f252a8ea1a571
渡邊直人 <naoto@watanabenaotonoMacBook-Air.local>
2024-02-05 01:07:36 +0900

    Update: spotify埋め込み

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
 import "@hotwired/turbo-rails"
 import "controllers"
 import Rails from "@rails/ujs";
 Rails.start();
 
 import "design_selection"
+import "posts"
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,16 @@ class Post < ApplicationRecord
   validates :design,  presence: true
 
   enum design: { red: 0, blue: 1, yellow: 2, green: 3 }
+
+
+  def split_id_from_spotify_url
+    # URLからクエリパラメータを除去し、必要なID部分のみを抽出
+    uri = URI.parse(link_to_music)
+    path_segments = uri.path.split('/')
+    # 例えば、"track"や"playlist"の後のセグメントをIDとして抽出
+    id_index = path_segments.index { |s| s == "track" || s == "playlist" } + 1
+    path_segments[id_index] if id_index.present? && path_segments.size > id_index
+  rescue URI::InvalidURIError
+    nil # URLが無効な場合はnilを返す
+  end
 end
