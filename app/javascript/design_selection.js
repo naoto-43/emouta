a33c30515f3dea14b15d5de5e5bd03af979b4d7e
渡邊直人 <naotwo28@gmail.com>
2024-02-05 02:30:06 +0900

    Fix: フォームに元々入力があった場合元々入力があった場合、jsが反映されていないバグの修正

--- app/javascript/design_selection.js
+++ app/javascript/design_selection.js
@@ -1,5 +1,5 @@
 document.addEventListener('turbo:load', function() {
-  // デザインオプションの変更時のイベント
+  // デザインオプションの変更時のイベントハンドラーを設定
   document.querySelectorAll('.design-option').forEach(function(element) {
     element.addEventListener('change', function() {
       // すべてのデザインラベルから 'selected-design' クラスを削除
@@ -9,20 +9,30 @@ document.addEventListener('turbo:load', function() {
       
       // 選択されたオプションの次のデザインラベルに 'selected-design' クラスを追加
       var nextLabel = this.nextElementSibling;
-      if (nextLabel.classList.contains('design-label')) {
+      if (nextLabel && nextLabel.classList.contains('design-label')) {
         nextLabel.classList.add('selected-design');
       }
     });
   });
 
-  // マウスオーバー時のイベント
+  // 画面ロード時に選択されているデザインオプションに基づいてラベルをハイライト
+  document.querySelectorAll('.design-option').forEach(function(option) {
+    if(option.checked) {
+      var nextLabel = option.nextElementSibling;
+      if (nextLabel && nextLabel.classList.contains('design-label')) {
+        nextLabel.classList.add('selected-design');
+      }
+    }
+  });
+
+  // マウスオーバー時のイベントハンドラーを設定
   document.querySelectorAll('.design-label').forEach(function(element) {
     element.addEventListener('mouseover', function() {
       this.classList.add('mouseover-style');
     });
   });
 
-  // マウスアウト時のイベント
+  // マウスアウト時のイベントハンドラーを設定
   document.querySelectorAll('.design-label').forEach(function(element) {
     element.addEventListener('mouseout', function() {
       this.classList.remove('mouseover-style');
--- app/javascript/posts.js
+++ app/javascript/posts.js
@@ -1,22 +1,27 @@
 document.addEventListener('turbo:load', function() {
   var linkInput = document.getElementById('link_to_music');
   var embedContainer = document.getElementById('spotify-embed-container');
-
-  linkInput.addEventListener('input', function() {
-    var url = this.value;
+  
+  function updateSpotifyEmbed(url) {
     var spotifyId = extractSpotifyId(url);
-    
     if (spotifyId) {
       var embedCode = `<iframe src="https://open.spotify.com/embed/track/${spotifyId}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
       embedContainer.innerHTML = embedCode;
     } else {
       embedContainer.innerHTML = '';
     }
-  });
+  }
 
   function extractSpotifyId(url) {
     var match = url.match(/(?:spotify:|https:\/\/[a-z]+\.spotify\.com\/)(track|playlist)\/([a-zA-Z0-9]+)/);
     return match ? match[2] : null;
   }
-});
 
+  // リンク入力フィールドの変更時に埋め込みを更新
+  linkInput.addEventListener('input', function() {
+    updateSpotifyEmbed(this.value);
+  });
+  
+  // ページロード時に既存のリンクをもとに埋め込みを表示
+  updateSpotifyEmbed(linkInput.value);
+});
