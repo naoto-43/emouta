f9fdabb7699f22fa4027908615cdb3e5275ff9f0
渡邊直人 <naoto@watanabenaotonoMacBook-Air.local>
2024-02-07 00:33:04 +0900

    Fix: css適応されないバグ修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,8 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
-  plugins: [require("daisyui")]
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes:["night"]
+  }
 }
