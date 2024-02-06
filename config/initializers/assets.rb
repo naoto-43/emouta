4d6e9118439997f2bca8ea02b0dadb833be90b55
渡邊直人 <naotwo28@gmail.com>
2024-02-07 06:55:58 +0900

    Fix: inter-font.cssをアセットぷりコンパイルのたアセットプリコンパイルの対象に追加

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.paths << Rails.root.join("node_modules/@fortawesome/fontawesome-free/webfonts")
 Rails.application.config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
+Rails.application.config.assets.precompile += %w( inter-font.css )
