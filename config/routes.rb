34f5feae1c5eec142968f84d8855d9061fe3403c
渡邊直人 <naoto@watanabenaotonoMacBook-Air.local>
2024-02-09 03:34:05 +0900

    Add: carrierwave,rails g uploader Avatar

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new create]
   resources :posts
+  resource :profile, only: %i[show edit update]
 end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -22,16 +22,6 @@ module.exports = {
     require('daisyui')
   ],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          // 用意されている各種の色も設定できる
-          primary: "#a991f7", // btn-primaryなどの背景の色
-          "primary-content": "#ffffff", // btn-primaryなどの文字の色
-          "base-100": "#16253D", // 全体の背景色
-          "base-content": "#EFB509", // 全体の文字の色
-        },
-      }
-    ],
+    themes: ["sunset"],
   }
 }
