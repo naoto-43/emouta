4faeb9c1fd5e1b85e437fda6afcbd36d42b135d7
渡邊直人 <naoto@watanabenaotonoMacBook-Air.local>
2024-02-09 05:50:35 +0900

    Add: profile機能、importmap削除前（flowbite使いたいのでesbuildに切り替え）

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,9 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
 import "@hotwired/turbo-rails"
 import "controllers"
 import Rails from "@rails/ujs"
 Rails.start();
 
+import "flowbite"
 import "header_adjustment"
 import "design_selection"
 // import "posts"
