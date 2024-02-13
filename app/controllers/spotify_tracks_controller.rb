ead7fdcd72ae2e45b0962f5d716c963302a6620d
渡邊直人 <naoto@watanabenaotonoMacBook-Air.local>
2024-02-13 16:11:50 +0900

    Update: 年代、ジャンルでtrack情報取得

--- app/controllers/spotify_tracks_controller.rb
+++ app/controllers/spotify_tracks_controller.rb
@@ -8,12 +8,31 @@ class SpotifyTracksController < ApplicationController
     if @spotify_track.save
       age_param = spotify_track_params[:age]
       genre_param = spotify_track_params[:genre]
-      tracks = RSpotify::Track.search('genre:#{genre_param} year:#{year_range}', limit: 10)
-      binding.pry
-      redirect_to root_path, success: t('defaults.message.created', item: SpotifyTrack.model_name.human)
-    else
-      flash.now['danger'] = t('defaults.message.not_created', item: SpotifyTrack.model_name.human)
+      year_range = case age_param
+      when 'upto_1979'
+        '1900-1979'
+      when 'the_1980s'
+        '1980-1989'
+      when 'the_1990s'
+        '1990-1999'
+      when 'the_2000s'
+        '2000-2009'
+      when 'the_2010s'
+        '2010-2019'
+      when 'from_2020'
+        '2020-'
+      else
+        '' # 未指定またはその他の場合
+      end
 
+      # 変数を展開してクエリを作成する
+      query = "genre:\"#{genre_param}\" year:#{year_range}"
+      tracks = RSpotify::Track.search(query, limit: 5)
+      tracks.each_with_index do |track, index|
+        track_link = track.external_urls['spotify']
+      end
+      redirect_to show_spotify_track_path, success: t('defaults.message.created', item: SpotifyTrack.model_name.human)
+    else
       render :new, status: :unprocessable_entity
     end
   end
