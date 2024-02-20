4f00ee2771266c2ea9a3795d84874c286e854fab
渡邊直人 <naotwo28@gmail.com>
2024-02-20 18:46:44 +0900

    Update: 入力したartist_nameもresult画面で表示するように変更表示するように変更

--- app/javascript/result_transition.js
+++ app/javascript/result_transition.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   var form = document.getElementById('artist_search_form');
   form.addEventListener('submit', function(event) {
-    event.preventDefault(); // フォームの送信を停止
-    var queryValue = document.getElementById('artist_query').value; // クエリの値を取得
-    // リダイレクト先のURLを設定
+    event.preventDefault();
+    var queryValue = document.getElementById('artist_query').value; 
     var redirectUrl = 'http://localhost:3000/spotify_tracks/result?query=' + encodeURIComponent(queryValue);
-    window.location.href = redirectUrl; // リダイレクトを実行
+    window.location.href = redirectUrl; 
   });
 });
