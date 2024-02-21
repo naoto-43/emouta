538609deb9661a9f1b4bd105daadbd32633f15b2
渡邊直人 <naoto@watanabenaotonoMacBook-Air.local>
2024-02-21 10:56:43 +0900

    Update: 楽曲紹介artist.nameのtext_field５つ表示（turbo flameは被って見えない）

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def artists
     query = params[:query]
+    @index = params[:index]
     @artists = RSpotify::Artist.search(query, limit: 5) if query.present?
     respond_to do |format|
       format.turbo_stream
