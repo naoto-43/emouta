c681c33469108fdcaf5bd400e0359fb122c7df98
渡邊直人 <naoto@watanabenaotonoMacBook-Air.local>
2024-02-21 18:44:58 +0900

    Update: ５件のアーティストからresult画面表示までできる

--- app/controllers/spotify_tracks_controller.rb
+++ app/controllers/spotify_tracks_controller.rb
@@ -4,25 +4,31 @@ class SpotifyTracksController < ApplicationController
   end
 
   def create
-    artist_name = params[:query]
-    artists = RSpotify::Artist.search(artist_name)
-
-    if artists.any?
-      seed_artists = artists.first.id
-      @recommendations = RSpotify::Recommendations.generate(limit: 10, seed_artists: [seed_artists])
+    artist_names = params[:query].values 
+    seed_artists = []
+  
+    artist_names.each do |artist_name|
+      artists = RSpotify::Artist.search(artist_name)
+  
+      seed_artists << artists.first.id if artists.any?
+      break if seed_artists.size >= 5 
+    end
+  
+    if seed_artists.empty?
+      flash.now[:alert] = "Artists not found. Please try again."
+      render :new, status: :unprocessable_entity
+    else
+      @recommendations = RSpotify::Recommendations.generate(limit: 10, seed_artists: seed_artists)
       if @recommendations.tracks.empty?
         flash.now[:alert] = "No recommendations found. Please try a different artist."
         render :new, status: :unprocessable_entity
       else
         session[:track_urls] = @recommendations.tracks.map { |track| track.external_urls['spotify'] }
-        session[:artist_name] = artist_name 
-        redirect_to spotify_track_result_path 
+        redirect_to spotify_track_result_path
       end
-    else
-      flash.now[:alert] = "Artist not found. Please try again."
-      render :new, status: :unprocessable_entity
     end
   end
+  
   
   def result
     @track_urls = session[:track_urls]
--- app/javascript/result_transition.js
+++ app/javascript/result_transition.js
@@ -2,8 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
   var form = document.getElementById('artist_search_form');
   form.addEventListener('submit', function(event) {
     event.preventDefault();
-    var queryValue = document.getElementById('artist_query').value; 
-    var redirectUrl = 'http://localhost:3000/spotify_tracks/result?query=' + encodeURIComponent(queryValue);
-    window.location.href = redirectUrl; 
+    window.location.href = 'http://localhost:3000/spotify_tracks/result'; 
   });
 });
