e472db30b9819af8b2c614544b51405112603ebe
渡邊直人 <naoto@watanabenaotonoMacBook-Air.local>
2024-03-01 00:35:32 +0900

    Update: 曲名候補の後ろにアーティスト名表示

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,7 +5,6 @@ import 'flowbite';
 // import "./design_selection";
 import "./embed_spotify";
 import "./header_adjustment";
-import "./result_transition";
 
 document.addEventListener("turbo:load", function() {
   console.log("Turbo frame loaded");
