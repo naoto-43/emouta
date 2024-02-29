511d6c861f15959c0fc8ee83fe4d6d5f0a07b90c
渡邊直人 <naotwo28@gmail.com>
2024-03-01 00:35:32 +0900

    Update: 曲名候補の後ろにアーティスト名表示

--- app/javascript/result_transition.js
+++
@@ -1,7 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   var form = document.getElementById('artist_search_form');
-//   form.addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     window.location.href = 'http://localhost:3000/spotify_tracks/result'; 
-//   });
-// });
