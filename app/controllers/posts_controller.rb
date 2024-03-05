efe05a0d4b423db3631c9408c8e4d654eb5095a3
渡邊直人 <naotwo28@gmail.com>
2024-03-05 14:38:21 +0900

    Update:ページネーション追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :new, :create, :destroy]
   def index
-    @posts = Post.all.includes(:user).order(created_at: :desc)
+    @posts = Post.all.includes(:user).order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def new
