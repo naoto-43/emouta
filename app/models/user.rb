6f77783f17d1297f69473015f5a8609ebde80d3e
渡邊直人 <naotwo28@gmail.com>
2024-03-20 02:31:31 +0900

    Update: nameカラムにバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :spotify_tracks
   mount_uploader :avatar, AvatarUploader
 
+  validates :name, presence: true
+
   def own?(object)
     id == object.user_id
   end
