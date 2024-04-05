7c23257be003ffc4c9af52270b927b4f69866910
渡邊直人 <naotwo28@gmail.com>
2024-04-06 00:37:41 +0900

    Update: コメントupdateアクション追加

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,13 +1,15 @@
 class PostCommentsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :edit, :updae]
+  before_action :authenticate_user!, only: [:create, :edit, :update]
 
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.post_comments.new(post_comment_params)
     @comment.user = current_user
     if @comment.save
+      binding.pry
       redirect_to @post, notice: 'Comment was successfully created.'
     else
+      binding.pry
       @comments = @post.post_comments.reload
       @new_comment = @comment
       render 'posts/show'
@@ -17,6 +19,17 @@ class PostCommentsController < ApplicationController
   def edit
     @post = Post.find(params[:post_id])
     @comment = @post.post_comments.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.post_comments.find(params[:id])
+  
+    if @comment.update(post_comment_params)
+      redirect_to @post, notice: 'Comment was successfully updated.'
+    else
+      render :edit
+    end
   end
   
   private
