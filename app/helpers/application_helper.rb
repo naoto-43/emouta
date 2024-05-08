032aec533ad56b5ef3923214736ab0a8c9c3d412
渡邊直人 <naotwo28@gmail.com>
2024-05-09 01:25:47 +0900

    Update: OGPの設定変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def default_meta_tags
+  site: 'emouta',
+  title:  'emouta - 「この曲のここが好き」を共有しよう！',
+  reverse: true,
+  charset: 'utf-8',
+  description: 'emoutaは掲示板と楽曲紹介の機能を持ったwebサービスです。好きな歌詞を共有したり、新たな楽曲を開拓してみませんか？',
+  keywords: 'emouta',
+  og: {
+    site_name: :site,
+    title: :title,
+    description: :description,  
+    type: 'website',
+    url: 'https://www.emouta.com/',
+    image: ActionController::Base.helpers.asset_url("投稿アイコン２.png", host: 'https://www.emouta.com')
+  }
 end
--- config/initializers/meta_tags.rb
+++ config/initializers/meta_tags.rb
@@ -1,23 +1,40 @@
-MetaTags.configure do |config|
-  config.title_limit        = 70
-  config.description_limit  = 160
-  config.keywords_limit     = 255
-  config.keywords_separator = ', '
-end
+# frozen_string_literal: true
 
-Rails.application.config.meta_tags = {
-  title:  'emouta - 「この曲のここが好き」を共有しよう！',
-  site: 'emouta',
-  reverse: true,
-  charset: 'utf-8',
-  description: 'emoutaは掲示板と楽曲紹介の機能を持ったwebサービスです。好きな歌詞を共有したり、新たな楽曲を開拓してみませんか？',
-  keywords: 'emouta',
-  og: {
-    site_name: "emouta",
-    title: 'emouta - 「この曲のここが好き」を共有しよう！',
-    description: 'emoutaで好きな歌詞を共有したり、新たな楽曲を開拓してみませんか？',
-    type: 'website',
-    url: 'https://www.emouta.com/',
-    image: ActionController::Base.helpers.asset_url("投稿アイコン２.png", host: 'https://www.emouta.com')
-  }
-}
+# Use this setup block to configure all options available in MetaTags.
+MetaTags.configure do |config|
+  # How many characters should the title meta tag have at most. Default is 70.
+  # Set to nil or 0 to remove limits.
+  # config.title_limit = 70
+
+  # When true, site title will be truncated instead of title. Default is false.
+  # config.truncate_site_title_first = false
+
+  # Maximum length of the page description. Default is 300.
+  # Set to nil or 0 to remove limits.
+  # config.description_limit = 300
+
+  # Maximum length of the keywords meta tag. Default is 255.
+  # config.keywords_limit = 255
+
+  # Default separator for keywords meta tag (used when an Array passed with
+  # the list of keywords). Default is ", ".
+  # config.keywords_separator = ', '
+
+  # When true, keywords will be converted to lowercase, otherwise they will
+  # appear on the page as is. Default is true.
+  # config.keywords_lowercase = true
+
+  # When true, the output will not include new line characters between meta tags.
+  # Default is false.
+  # config.minify_output = false
+
+  # When false, generated meta tags will be self-closing (<meta ... />) instead
+  # of open (`<meta ...>`). Default is true.
+  # config.open_meta_tags = true
+
+  # List of additional meta tags that should use "property" attribute instead
+  # of "name" attribute in <meta> tags.
+  # config.property_tags.push(
+  #   'x-hearthstone:deck',
+  # )
+end
