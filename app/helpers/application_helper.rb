c93cd8f5ef2a3b80570b37cf8f4278b6eefcbf2b
渡邊直人 <naotwo28@gmail.com>
2024-05-09 03:17:12 +0900

    update: default_meta_tags修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,28 @@
 module ApplicationHelper
   def default_meta_tags
-  site: 'emouta',
-  title:  'emouta - 「この曲のここが好き」を共有しよう！',
-  reverse: true,
-  charset: 'utf-8',
-  description: 'emoutaは掲示板と楽曲紹介の機能を持ったwebサービスです。好きな歌詞を共有したり、新たな楽曲を開拓してみませんか？',
-  keywords: 'emouta',
-  og: {
-    site_name: :site,
-    title: :title,
-    description: :description,  
-    type: 'website',
-    url: 'https://www.emouta.com/',
-    image: ActionController::Base.helpers.asset_url("投稿アイコン２.png", host: 'https://www.emouta.com')
-  }
+    {
+      site: 'emouta',
+      title:  'emouta - 「この曲のここが好き」を共有しよう！',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'emoutaは掲示板と楽曲紹介の機能を持ったwebサービスです。好きな歌詞を共有したり、新たな楽曲を開拓してみませんか？',
+      keywords: 'emouta',
+      separator: '|',
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,  
+        type: 'website',
+        canonical: request.original_url,
+        image: image_url('投稿アイコン２.png')
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: :site,
+        image: image_url('投稿アイコン２.png')
+      }
+    }
+  end
 end
