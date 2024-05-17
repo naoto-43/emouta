92f7375d5f4f07ceefaf31706928ce68accd97f2
渡邊直人 <naotwo28@gmail.com>
2024-05-17 09:08:03 +0900

    Update: コメントturbo stream化

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,15 +3,20 @@ class PostCommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.post_comments.new(post_comment_params)
-    @comment.user = current_user
-    if @comment.save
-      redirect_to @post, success: t('defaults.message.created', item: PostComment.model_name.human)
+    @new_comment = @post.post_comments.new(post_comment_params)
+    @new_comment.user = current_user
+
+    if @new_comment.save
+      flash[:success] = t('defaults.message.created', item: PostComment.model_name.human)
+      @comments = @post.post_comments.reload
+      respond_to do |format|
+        format.turbo_stream { render 'create', locals: { post: @post, new_comment: PostComment.new } }
+        format.html { redirect_to @post }
+      end
     else
       @comments = @post.post_comments.reload
-      @new_comment = @comment
       flash.now[:danger] = t('defaults.message.not_created', item: PostComment.model_name.human)
-      render 'posts/show'
+      render 'posts/show', status: :unprocessable_entity
     end
   end
 
@@ -47,6 +52,6 @@ class PostCommentsController < ApplicationController
   private
 
   def post_comment_params
-    params.require(:post_comment).permit(:comment)
+    params.require(:post_comment).permit(:comment, :post_id)
   end
 end
