c6d910e615f6336088ee209d041ae79323a52e28
渡邊直人 <naotwo28@gmail.com>
2024-05-18 10:15:33 +0900

    Update: pumaのワーカー数削減

--- config/puma.rb
+++ config/puma.rb
@@ -14,8 +14,7 @@ threads min_threads_count, max_threads_count
 # Specifies that the worker count should equal the number of processors in production.
 if ENV['RAILS_ENV'] == 'production'
   require 'concurrent-ruby'
-  worker_count = Integer(ENV.fetch('WEB_CONCURRENCY') { Concurrent.physical_processor_count })
-  workers worker_count if worker_count > 1
+  workers Integer(ENV.fetch('WEB_CONCURRENCY') { 2 })
 end
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
